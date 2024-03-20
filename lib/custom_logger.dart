e524f40d88f9984dad0df74513b516a445d391e7
Dariusz Mikulski <dariusz.mikulski@gmail.com>
2024-03-20 11:18:22 +0100

    Fix initialization

--- lib/custom_logger.dart
+++ lib/custom_logger.dart
@@ -12,7 +12,7 @@ class CustomLogger {
 
   factory CustomLogger(String loggerName, log.LogOutput? output) {
     _customLogger.loggerName ??= loggerName;
-    _customLogger.internalLogger = log.Logger(
+    _customLogger.internalLogger ??= log.Logger(
       printer: log.SimplePrinter(printTime: true),
       output: output ?? log.ConsoleOutput(),
     );
@@ -23,35 +23,35 @@ class CustomLogger {
 
   static final CustomLogger _customLogger = CustomLogger._internal();
 
-  late final log.Logger internalLogger;
+  log.Logger? internalLogger;
 
   void t(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    internalLogger.t('$loggerName: $message',
+    internalLogger?.t('$loggerName: $message',
         error: error, stackTrace: stackTrace);
   }
 
   void d(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    internalLogger.d('$loggerName: $message',
+    internalLogger?.d('$loggerName: $message',
         error: error, stackTrace: stackTrace);
   }
 
   void i(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    internalLogger.i('$loggerName: $message',
+    internalLogger?.i('$loggerName: $message',
         error: error, stackTrace: stackTrace);
   }
 
   void w(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    internalLogger.w('$loggerName: $message',
+    internalLogger?.w('$loggerName: $message',
         error: error, stackTrace: stackTrace);
   }
 
   void e(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    internalLogger.e('$loggerName: $message',
+    internalLogger?.e('$loggerName: $message',
         error: error, stackTrace: stackTrace);
   }
 
   void f(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    internalLogger.f('$loggerName: $message',
+    internalLogger?.f('$loggerName: $message',
         error: error, stackTrace: stackTrace);
   }
 }
